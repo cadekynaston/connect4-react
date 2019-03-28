29b9a3f1c551a0fdb5aee912fcbf3d14e3253dd8
Cade Kynaston <cade.kynaston@clearlink.com>
2019-03-28 13:06:22 -0600

    Updates

--- precache-manifest.ff1248713b6e5446d4918032e7c978e1.js
+++ precache-manifest.ff1248713b6e5446d4918032e7c978e1.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/connect4-react/static/js/runtime~main.684ca7db.js"
   },
   {
-    "revision": "536993b2ed5af2f49382",
-    "url": "/connect4-react/static/js/main.985c0f71.chunk.js"
+    "revision": "e5c15bcb3fe0b13df6d4",
+    "url": "/connect4-react/static/js/main.c350ae71.chunk.js"
   },
   {
     "revision": "fc6336a6793301bd3752",
     "url": "/connect4-react/static/js/2.6268aeda.chunk.js"
   },
   {
-    "revision": "536993b2ed5af2f49382",
-    "url": "/connect4-react/static/css/main.68f58759.chunk.css"
+    "revision": "e5c15bcb3fe0b13df6d4",
+    "url": "/connect4-react/static/css/main.baa99f4d.chunk.css"
   },
   {
-    "revision": "d6e86cd5e083f5258a55dcdf0d07d3ea",
+    "revision": "b3fc4d94d17a29d1accc1e6292b38e33",
     "url": "/connect4-react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/connect4-react/precache-manifest.3c0c4390f9acf30f638c26aa588566ee.js"
+  "/connect4-react/precache-manifest.ff1248713b6e5446d4918032e7c978e1.js"
 );
 
 workbox.clientsClaim();
