60e5ae76ec5a4e48d42b9e26282b727a11b2ec6c
Cade Kynaston <cade.kynaston@clearlink.com>
2019-03-31 14:16:44 -0600

    Updates

---
+++ precache-manifest.d9043b70625f7289aed79ee35ee10eb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b37f1ca89d1f21612c2a",
+    "url": "/connect4-react/static/js/runtime~main.684ca7db.js"
+  },
+  {
+    "revision": "9b0e903c332fb2561734",
+    "url": "/connect4-react/static/js/main.d0723ad5.chunk.js"
+  },
+  {
+    "revision": "3467012176ee4707b885",
+    "url": "/connect4-react/static/js/2.56c3fe2b.chunk.js"
+  },
+  {
+    "revision": "9b0e903c332fb2561734",
+    "url": "/connect4-react/static/css/main.997698a4.chunk.css"
+  },
+  {
+    "revision": "8f821c05997a7a0f381be35da96363da",
+    "url": "/connect4-react/index.html"
+  }
+];
